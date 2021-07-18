ea242e1f3d3abd1e075bf3181146f75d637e784f
JuYongWoo <72632207+dyddndn@users.noreply.github.com>
2021-07-18 17:26:26 +0900

    bug fix
    bug fix

--- Doteater/Assets/Script/Player.cs
+++ Doteater/Assets/Script/Player.cs
@@ -14,6 +14,8 @@ public class Player : MonoBehaviour
     float nowtime = 0.0f;
     float thattime = 0.0f;
     float sprinttime = 0.0f;
+    float sprinttime2 = 0.0f;
+    bool sprinting = false;
     float sprintcooling = 3.0f;
     public float sprintcooltime = 3.0f;
     float staminacooltime = 0.0f;
@@ -36,12 +38,12 @@ public class Player : MonoBehaviour
     {
         if(this.gameObject.name=="Player") {
         Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        if (dir.sqrMagnitude > 0.01f)
+        if (dir.sqrMagnitude > 0.001f)
         {
-            Vector3 forward = Vector3.Slerp(transform.forward, dir,rotationSpeed * Time.deltaTime / Vector3.Angle(transform.forward, dir));
-            transform.LookAt(transform.position + forward);
+            Vector3 forward = Vector3.Slerp(transform.forward, dir,rotationSpeed * Time.deltaTime  / Vector3.Angle(transform.forward, dir));
+            transform.LookAt(transform.position + forward * 2);
         }
-        charCtrl.Move(dir * moveSpeed * Time.deltaTime);
+        charCtrl.SimpleMove(dir * moveSpeed * 50 * Time.deltaTime);
         anim.SetFloat("Speed", charCtrl.velocity.magnitude); // Speed라는 이름의 float에 velocity.magnitude를 set한다.
 
         if (GameObject.FindGameObjectsWithTag("Dot").Length < 1){
@@ -51,14 +53,11 @@ public class Player : MonoBehaviour
         }
     }
         //Shift키를 누를 경우
-        if(Input.GetKey(KeyCode.LeftShift))
+        if(Input.GetKey(KeyCode.LeftShift) && stamina >0)
         {
-            if (stamina > 0)
-            {
                 moveSpeed = 10;
-                stamina -= Time.deltaTime * 30;
+                stamina -= Time.deltaTime * 20;
                 staminacooltime = 0.0f;
-            }
         }
         else
         {
@@ -67,8 +66,6 @@ public class Player : MonoBehaviour
             staminacooltime += Time.deltaTime;
             if(staminacooltime>1)
             stamina += Time.deltaTime * 10;
-            if (stamina > 100)
-                stamina = 100;
         }
 
         //맞는 딜레이
@@ -80,13 +77,12 @@ public class Player : MonoBehaviour
 
 
         //Ctrl키를 누를 경우
-        if (Input.GetKeyDown(KeyCode.LeftControl) && sprintcooling > sprintcooltime)
+        if (Input.GetKeyDown(KeyCode.LeftControl) && sprintcooling > sprintcooltime && stamina >30)
         {
-            sprinttime = nowtime;
-            anim.SetBool("Sprint", true);
-            Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            charCtrl.Move(dir * 5);
-            stamina -= 50;
+            sprinttime2 = nowtime+0.3f;
+            sprinting = true;
+
+            stamina -= 30;
             staminacooltime = 0.0f;
             sprintcooling = 0;
 
@@ -96,11 +92,30 @@ public class Player : MonoBehaviour
             sprintcooling += Time.deltaTime;
 
         }
+
+        if(sprinttime2>nowtime && sprinting)
+        {
+            anim.SetBool("Sprint", true);
+            Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            charCtrl.Move(dir * 8 * Time.deltaTime);
+        }
+        else
+        {
+            sprinting = false;
+        }
+
+
         //스프린트 모션 복구 딜레이
-        if (nowtime - sprinttime > 0.5)
+        if (sprinttime2<nowtime)
         {
             anim.SetBool("Sprint", false);
         }
+
+
+        if (stamina > 100)
+            stamina = 100;
+        if (stamina < 0)
+            stamina = 0;
 
         nowtime += Time.deltaTime; //nowtime은 게임 시작 경과시간
     }
